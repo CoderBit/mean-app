d482c4251bc05858d8e59c8882a0235a775eeb31
CobderBit <himanshu95.2010@gmail.com>
2019-11-05 16:51:24 +0530

    Retain post details in edit post on refresh

--- src/app/posts/post.service.ts
+++ src/app/posts/post.service.ts
@@ -36,7 +36,7 @@ export class PostService {
   }
 
   getPost(id: string){
-    return {...this.posts.find(p => p.id === id)};
+    return this.http.get<{_id: string, title: string, content: string}>(`http://localhost:3000/api/posts/${id}`);
   }
 
   addPost(title: string, content: string){
