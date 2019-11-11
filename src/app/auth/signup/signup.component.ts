887fd0b4d5e2ba5d9472cf6237700b7d8d745094
CobderBit <himanshu95.2010@gmail.com>
2019-11-11 12:04:20 +0530

    added signup & login backend code and generated json webtoken
    added signup & login backend code and connecting to frontend
    Added jsonwebtoken package
    generated json webtoken
    Added auth service

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-signup',
@@ -10,7 +11,7 @@ export class SignUpComponent implements OnInit {
 
   isLoading = false;
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
   }
@@ -19,7 +20,7 @@ export class SignUpComponent implements OnInit {
     if(form.invalid){
       return;
     }
+    this.authService.createUser(form.value.email, form.value.password);
 
-    
   }
 }
